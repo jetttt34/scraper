4d84f9ea3d6022b93275da953cea383260bfc848
Jed garza <Jedgarza@gmail.com>
2019-10-28 21:26:08 -0500

    Added routed still to many bugs

--- server.js
+++ server.js
@@ -1,43 +1,41 @@
-var express = require("express");
-var exphbs  = require('express-handlebars');
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-// const cheerio =require("cheerio");
-// const axios = require('axios');
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser"); //JSON responses
+const mongoose = require("mongoose"); //Mongo object modelling 
+const request = require("request"); //Makes http calls
+const cheerio = require("cheerio"); //Scraper
 
+// Require all models
+const db = require("../models");
 
-var PORT = process.env.PORT  3000;
+// Port configuration for local/Heroku
+const PORT = process.env.PORT || process.argv[2] || 8080;
 
-var app = express();
-var router = express.Router;
-require("/routes")(router);
+// Initialize Express
+const app = express();
 
-app.use(express.static(__dirname +"/public"));
-// 
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main"
-}));
-app.set("view engine","handlebars");
-// 
+// Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+// Controllers
+const router = require("./controllers/api.js");
 app.use(router);
-var db = process.env.MONGODB_URI  "mongodb://localhost/mongoHeadlines";
-mongoose.connect(db, function(err){
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log("mongoose connection successful");
-    }
-});
+// Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
-
-app.listen(PORT, function(){
-    console.log("listening on port:"+ PORT);
+// Start the server
+app.listen(PORT, function () {
+    console.log(`This application is running on port: ${PORT}`);
 });
